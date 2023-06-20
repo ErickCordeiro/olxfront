a665a3f8a8d91ebae544b78b3459c1cec5cae933
Erick Cordeiro <erick.arruda.ext@infracommerce.com.br>
2023-06-20 14:46:14 -0300

    Criado a estrutura do menu principal do projeto

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { HeaderArea } from './styles.js';
+// import logo from './../../../../public/olx-logo.png';
 
 const Header = () => {
     return(
-        <HeaderArea>Header</HeaderArea>
+        <HeaderArea>
+            <div className="header-container">
+                <div className='header-container__logo'>
+                    <Link to="/">
+                        <img className="header-container__logo__img" src={process.env.PUBLIC_URL+'/olx-logo.png'} alt='Olx Clone' />
+                    </Link>
+                </div>
+                <nav className='header-container__menu'>
+                    <ul className='header-container__menu__nav'>
+                        <li className='header-container__menu__nav__link'><Link to="">Login</Link></li>
+                        <li className='header-container__menu__nav__link'><Link to="">Cadastre-se</Link></li>
+                        <li className='header-container__menu__nav__link'><Link to="" className='header-container__menu__nav__button'>Anúncie aqui</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </HeaderArea>
     );
 }
 
--- src/components/partials/Header/styles.js
+++ src/components/partials/Header/styles.js
@@ -1,6 +1,61 @@
 import styled from 'styled-components';
 
 export const HeaderArea = styled.div`
-    height: 100px;
-    background-color: #ccc;
+    background-color: #fff;
+    padding: 10px 0;
+    border-bottom: 1px solid #cccccc;
+    height: 60px;
+
+    .header-container {
+        max-width: 1280px;
+        margin: 0 auto;
+        padding: 0 2%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .header-container__logo__img {
+        width: 60px;
+    }
+
+    nav {
+        padding: 10px 0;
+
+        ul, li {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+
+        ul {
+            display: flex;
+            align-items: center;
+        }
+
+        li {
+            margin: 0 20px;
+
+            a {
+                text-decoration: none;
+                color: #000;
+                font-size: 14px;
+
+                &:hover {
+                    color: #EC7F19;
+                }
+
+                &.header-container__menu__nav__button {
+                    background-color: #EC7F19;
+                    border-radius: 4px;
+                    color: #fff;
+                    padding: 5px 20px;
+
+                    &:hover {
+                        background-color: #c9670c;
+                    }
+                }
+            }
+        }
+    }
 `;
